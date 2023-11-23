f2ebb2a6d7843d6faf5e09540fd993bec694731e
programmer-kabir <programmerkabirr@gmail.com>
2023-11-23 23:51:55 +0600

    simple validate

--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -8,24 +8,27 @@ const OrdersSchema = new Schema<Orders>({
 });
 
 const AddressSchema = new Schema<Address>({
-  street: { type: String, required: true },
+  street: { type: String, required: true, },
   city: { type: String, required: true },
-  country: { type: String, required: true },
+  country: { type: String, required: true,trim:true },
 });
 
 const userSchema = new Schema<User>({
-  userId: { type: Number, required: true },
-  username: { type: String, required: true },
-  password: { type: String, required: true },
+  userId: { type: Number, required: [true, 'ID is required'], unique:true },
+  username: { type: String, required: [true, 'User Name is required'],unique:true },
+  password: { type: String, required: [true, 'Password is required'], },
   fullName: {
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
+    firstName: { type: String, required: [true, 'First Name is required'], trim:true },
+    lastName: { type: String, required: [true, 'Last Name is required'],trim:true },
   },
-  age: { type: Number, required: true },
-  isActive: { type: Boolean, required: true },
-  hobbies: { type: [String], required: true },
-  address: AddressSchema,
-  email: { type: String, required: true },
+  age: { type: Number, required: [true, 'Age is required'], },
+  isActive: { type: Boolean, required: [true, 'isActive is required'], },
+  hobbies: { type: [String], required: [true, 'Hobbies is required'], },
+  address: {
+    type: AddressSchema,
+    required: [true, 'Address is required'],
+  },
+  email: { type: String, required: [true, 'Email is required'], unique: true, },
   orders: [OrdersSchema],
 });
 
