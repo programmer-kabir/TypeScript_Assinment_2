cccd922cf519df6b0203f5c5760f38aac67c30c4
programmer-kabir <programmerkabirr@gmail.com>
2023-11-23 21:51:14 +0600

    server create successfully

--- src/server.ts
+++ src/server.ts
@@ -3,13 +3,11 @@ import config from "./app/config";
 
 import mongoose from "mongoose";
 
-main().catch((err) => console.log(err));
-
 async function main() {
   try {
     await mongoose.connect(config.database_url as string);
     app.listen(config.port, () => {
-      console.log(`Example app listening on port ${config.port}`);
+      console.log(`server ok ${config.port}`);
     });
   } catch (error) {
     console.log(error);
